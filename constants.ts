84a7d04f12245adeff27a38982e76b6a0a75c521
TheSamael <116261317+TheSamael@users.noreply.github.com>
2026-01-30 19:13:18 +0530

    Update constants.ts
    Privacy and security changes

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 import { BaseCurrency } from './types';
 
-export const API_KEY = 'c3fed060ca825ebaa0581a04';
+export const API_KEY = 'your_api_goes_here';
 export const API_BASE_URL = 'https://v6.exchangerate-api.com/v6';
 
 export const DEFAULT_BASE_CURRENCY = BaseCurrency.INR;
